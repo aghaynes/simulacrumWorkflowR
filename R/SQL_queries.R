74b48ae01fe2a59076d0ce2a490a2f84b1cc0e69
p90j <jakob1741@hotmail.com>
2024-11-15 14:37:05 +0100

    almost done with the sql setup and with some helper functions. I have also started to do some visualisations. I might have to spend more time on playing around in Simulacrum to provide even more helper functions. I've also tested some packages from JOSS to get a idea of the entrance level

--- R/SQL_queries.R
+++ R/SQL_queries.R
@@ -5,18 +5,35 @@ library(tcltk)
 
 dir <- "C:/Users/p90j/Desktop/Jakob/Data/Simulacrum/simulacrum_v2.1.0/Data/"
 
-# Reading the csv files
-sim_av_patient <- read.csv(paste0(dir, "sim_av_patient.csv"))
-sim_av_tumour <- read.csv(paste0(dir, "sim_av_tumour.csv"))
-sim_av_gene <- read.csv(paste0(dir, "sim_av_gene.csv"))
-sim_rtds_combined <- read.csv(paste0(dir, "sim_rtds_combined.csv"))
-sim_sact_outcome <- read.csv(paste0(dir, "sim_sact_outcome.csv"))
-sim_sact_regimen <- read.csv(paste0(dir, "sim_sact_regimen.csv"))
-sim_sact_cycle <- read.csv(paste0(dir, "sim_sact_cycle.csv"))
-sim_sact_drug_detail <- read.csv(paste0(dir, "sim_sact_drug_detail.csv"))
+read_all_csv <- function(dir)
+{
+  print("Please wait. It can take a couple of minutes to upload all files ")
+  sim_av_patient <- read.csv(paste0(dir, "sim_av_patient.csv"))
+  print("sim_av_patient is uploaded")
+  sim_av_tumour <- read.csv(paste0(dir, "sim_av_tumour.csv"))
+  print("sim_av_tumours is uploaded")
+  sim_av_gene <- read.csv(paste0(dir, "sim_av_gene.csv"))
+  print("sim_av_gene is uploaded")
+  sim_rtds_combined <- read.csv(paste0(dir, "sim_rtds_combined.csv"))
+  print("sim_rtds_combined is uploaded")
+  sim_sact_outcome <- read.csv(paste0(dir, "sim_sact_outcome.csv"))
+  print("sim_sact_outcome is uploaded")
+  sim_sact_regimen <- read.csv(paste0(dir, "sim_sact_regimen.csv"))
+  print("sim_sact_regimen is uploaded")
+  sim_sact_cycle <- read.csv(paste0(dir, "sim_sact_cycle.csv"))
+  print("sim_sact_cycle is uploaded")
+  sim_sact_drug_detail <- read.csv(paste0(dir, "sim_sact_drug_detail.csv"))
+  print("sim_sact_drug_detail is uploaded")
+  print("All CSV files is succesfully uploaded!")
+  
+}
+
+read_all_csv("C:/Users/p90j/Desktop/Jakob/Data/Simulacrum/simulacrum_v2.1.0/Data/")
+
+
 
 sql_sim_av_patient <- table_sample(sim_av_patient)
-sql_sim_av_tumour <- table_sample(sim_av_patient)
+sql_sim_av_tumour <- table_sample(sim_av_tumour)
 sql_sim_av_gene <- table_sample(sim_av_gene)
 
 
@@ -30,56 +47,67 @@ sql_sim_rtds_combined <- table_sample(sim_rtds_combined)
 sql_sim_av_patient
 sql_sim_av_tumour
 
-join_string <- "
-SELECT *
-FROM sql_sim_av_patient
-INNER JOIN sql_sim_av_tumour ON sql_sim_av_patient.patientid = sql_sim_av_tumour.patientid
-INNER JOIN sql_sim_av_gene ON sql_sim_av_tumour.patientid = sql_sim_av_gene.patientid;
-"
 
-# Resultant data frame
-full_av <- sqldf(join_string, 
-                            stringsAsFactors = FALSE)
-head(full_av)
-colnames(full_av)
+########## sql_test setup ###########
+sql_test <- function(query) {
+  sqldf(query, stringsAsFactors = FALSE)
+}
 
+sql_test("SELECT PATIENTID, GENDER, AGE 
+ FROM sql_sim_av_tumour 
+  
+ WHERE age > 50")
 
-# Test queries 
-join_string <- "
-SELECT *
-FROM sql_sim_sact_regimen
-INNER JOIN sql_sim_sact_outcome ON sql_sim_sact_regimen.merged_regimen_id = sql_sim_sact_outcome.merged_regimen_id
-INNER JOIN sql_sim_sact_cycle ON sql_sim_sact_regimen.merged_regimen_id = sql_sim_sact_cycle.merged_regimen_id
-INNER JOIN sql_sim_sact_drug_detail ON sql_sim_sact_cycle.merged_cycle_id = sql_sim_sact_drug_detail.merged_cycle_id;
-
-"
-
-# Resultant data frame
-full_sact <- sqldf(join_string, 
-                            stringsAsFactors = FALSE)
-head(full_sact)
-colnames(full_sact)
+sql_test("SELECT sql_sim_av_tumour.PATIENTID, sql_sim_av_patient.ETHNICITY 
+ FROM sql_sim_av_tumour 
+ INNER JOIN sql_sim_av_patient ON sql_sim_av_tumour.PATIENTID = sql_sim_av_patient.PATIENTID")
 
 
+###### Test sql builder #######
+query_constructor <- function(tables, vars = "*", filters = NULL, joins = NULL) {
+  # SELECT
+  select_query <- paste("SELECT", paste(vars, collapse = ", "))
+  
+  # FROM
+  from_query <- paste("FROM", tables)
+  
+  # JOIN
+  join_query <- if (!is.null(joins)) {
+    paste(joins, collapse = " ")
+  } else {
+    ""
+  }
+  
+  # FILTERS
+  where_query <- if (!is.null(filters)) {
+    paste("WHERE", paste(filters, collapse = " AND "))
+  } else {
+    ""
+  }
+  
+  # Combine all clauses
+  query <- paste(select_query, '\n',
+                 from_query, '\n',
+                 join_query, '\n',
+                 where_query) 
+  
+  # Print the formatted query
+  writeLines(query)
+}
 
 
-# Test queries 
-join_string2 <- "
-SELECT *
-FROM sql_sim_av_patient
-INNER JOIN sql_sim_av_tumour ON sql_sim_av_patient.patientid = sql_sim_av_tumour.patientid
-INNER JOIN sql_sim_av_gene ON sql_sim_av_tumour.patientid = sql_sim_av_gene.patientid
-INNER JOIN sql_sim_sact_regimen ON sql_sim_av_patient.patientid = sql_sim_sact_regimen.encore_patient_id
-INNER JOIN sql_sim_sact_outcome ON sql_sim_sact_regimen.merged_regimen_id = sql_sim_sact_outcome.merged_regimen_id
-INNER JOIN sql_sim_sact_cycle ON sql_sim_sact_regimen.merged_regimen_id = sql_sim_sact_cycle.merged_regimen_id
-INNER JOIN sql_sim_sact_drug_detail ON sql_sim_sact_cycle.merged_cycle_id = sql_sim_sact_drug_detail.merged_cycle_id
-INNER JOIN sql_sim_rtds_combined ON sql_sim_av_patient.patientid = sql_sim_rtds_combined.patientid;
-"
+query1 <- query_constructor(tables = "sql_sim_av_tumour", 
+                            vars = c("PATIENTID", "GENDER", "AGE"), 
+                            filters = c("age > 50"))
 
-# Resultant data frame
-full_data <- sqldf(join_string2, 
-                            stringsAsFactors = FALSE)
-colnames(full_data)
+
+query2 <- query_constructor(
+  table = "sql_sim_av_tumour",
+  vars = c("sql_sim_av_tumour.PATIENTID", "sql_sim_av_patient.ETHNICITY"),
+  joins = c("INNER JOIN sql_sim_av_patient ON sql_sim_av_tumour.PATIENTID = sql_sim_av_patient.PATIENTID"),
+  
+)
+
 
 
 ######## Single tables query ########## 
@@ -191,8 +219,7 @@ FROM
 
 sql_full <- function()
 {
-  '
-SELECT *
+  writeLines('SELECT *
 FROM sql_sim_av_patient
 INNER JOIN sim_av_tumour ON sim_av_patient.patientid = sim_av_tumour.patientid
 INNER JOIN sim_av_gene ON sim_av_tumour.patientid = sim_av_gene.patientid
@@ -201,5 +228,64 @@ INNER JOIN sim_sact_outcome ON sim_sact_regimen.merged_regimen_id = sim_sact_out
 INNER JOIN sim_sact_cycle ON sim_sact_regimen.merged_regimen_id = sim_sact_cycle.merged_regimen_id
 INNER JOIN sim_sact_drug_detail ON sim_sact_cycle.merged_cycle_id = sim_sact_drug_detail.merged_cycle_id
 INNER JOIN sim_rtds_combined ON sim_av_patient.patientid = sim_rtds_combined.patientid;
-'
+')
 }
+
+
+
+###### Test sql builder #######
+query_constructor <- function(tables, vars = "*", filters = NULL, joins = NULL) {
+  # SELECT
+  select_query <- paste("SELECT", paste(vars, collapse = ", "))
+  
+  # FROM
+  from_query <- paste("FROM", tables)
+  
+  # JOIN
+  join_query <- if (!is.null(joins)) {
+    paste(joins, collapse = " ")
+  } else {
+    ""
+  }
+  
+  # FILTERS
+  where_query <- if (!is.null(filters)) {
+    paste("WHERE", paste(filters, collapse = " AND "))
+  } else {
+    ""
+  }
+  
+  # Combine all clauses
+  query <- paste(select_query, '\n',
+                 from_query, '\n',
+                 join_query, '\n',
+                 where_query) 
+  
+  # Print the formatted query
+  writeLines(query)
+}
+
+
+query1 <- query_constructor(tables = "sql_sim_av_tumour", 
+                            vars = c("PATIENTID", "GENDER", "AGE"), 
+                            filters = c("age > 50"))
+
+
+query2 <- query_constructor(
+  table = "sql_sim_av_tumour",
+  vars = c("sql_sim_av_tumour.PATIENTID", "sql_sim_av_patient.ETHNICITY"),
+  joins = c("INNER JOIN sql_sim_av_patient ON sql_sim_av_tumour.PATIENTID = sql_sim_av_patient.PATIENTID"),
+
+)
+
+
+
+
+############ Setup a database function to run queries in R ################
+# Just make the sqldf function up as a function with the sql query builder 
+
+
+
+
+
+
--- SimulacrumEDA.R
+++ SimulacrumEDA.R
@@ -1,7 +1,16 @@
 source("R/preprocessing.R")
+source("R/SQL_queries.R")
 source("R/time_management.R")
 # Record the start time
 start_time <- Sys.time()
+
+
+
+sql_test("
+SELECT *
+FROM sql_sim_av_patient
+INNER JOIN sql_sim_av_tumour ON sql_sim_av_patient.patientid = sql_sim_av_tumour.patientid
+INNER JOIN sql_sim_av_gene ON sql_sim_av_tumour.patientid = sql_sim_av_gene.patientid;")
 
 
 
@@ -28,6 +37,7 @@ sim_av_patient_sample <- table_sample(sim_av_patient)
 sim_sact_outcome_sample <- table_sample(sim_sact_outcome)
 summary(sim_sact_outcome_sample)
 
+sim_av_patient_sample <- table_sample(sim_av_patient, 5)
 
 
 colnames(sim_av_patient_sample)
@@ -45,5 +55,3 @@ skim(sim_av_patient_sample)
 
 end_time <- Sys.time()
 compute_time_limit(execution_time)
-
-sql_sim_av_patient()
