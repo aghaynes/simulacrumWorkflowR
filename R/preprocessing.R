8a106f9b9333c3754616ea6d8e11f866f0ec7006
p90j <jakob1741@hotmail.com>
2024-11-25 13:55:12 +0100

    made some additional funcitons to the preprocessing and started on writing documentation

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -5,10 +5,6 @@ table_sample <- function(df, n = 1000) {
 }
 
 
-###########################
-### Lars code work on it ##
-###########################
-
 
 cancer_grouping <- function(df) {
   df %>%
@@ -34,20 +30,145 @@ cancer_grouping <- function(df) {
     select(-ip)  # Remove the temporary 'ip' column
 }
 
-group_by_age <- function(df, age_column = "AGE") {
-  if (age_column %in% names(df)) {
+group_by_age <- function(df, age = "AGE") {
+  if (age %in% names(df)) {
     df$Grouped_Age <- dplyr::case_when(
-      dplyr::between(df[[age_column]], 18, 44)  ~ "18-44",
-      dplyr::between(df[[age_column]], 45, 64)  ~ "45-64",
-      dplyr::between(df[[age_column]], 65, 74)  ~ "65-74",
-      dplyr::between(df[[age_column]], 75, 130) ~ "> 75",
+      dplyr::between(df[[age]], 18, 44)  ~ "18-44",
+      dplyr::between(df[[age]], 45, 64)  ~ "45-64",
+      dplyr::between(df[[age]], 65, 74)  ~ "65-74",
+      dplyr::between(df[[age]], 75, 130) ~ "> 75",
       TRUE                                      ~ "Outside range"
     )
   } else {
-    warning(paste("Column", age_column, "not found in the data frame."))
+    warning(paste("Column", age, "not found in the data frame."))
   }
   return(df)
 }
 
 
+group_ethnicity <- function(df) {
+  ethnicity_mapping <- c(
+    "A" = "White", 
+    "B" = "White", 
+    "C" = "White", 
+    "C2" = "White", 
+    "C3" = "White",
+    "CA" = "White", 
+    "CB" = "White", 
+    "CC" = "White", 
+    "CD" = "White", 
+    "CE" = "White",
+    "CF" = "White", 
+    "CG" = "White", 
+    "CH" = "White", 
+    "CJ" = "White", 
+    "CK" = "White",
+    "CL" = "White", 
+    "CM" = "White", 
+    "CN" = "White", 
+    "CP" = "White", 
+    "CQ" = "White",
+    "CR" = "White", 
+    "CS" = "White", 
+    "CT" = "White", 
+    "CU" = "White", 
+    "CV" = "White",
+    "CW" = "White", 
+    "CX" = "White", 
+    "CY" = "White", 
+    "D" = "Mixed", 
+    "E" = "Mixed",
+    "F" = "Mixed", 
+    "G" = "Mixed", 
+    "GA" = "Mixed", 
+    "GB" = "Mixed", 
+    "GC" = "Mixed",
+    "GD" = "Mixed", 
+    "GE" = "Mixed", 
+    "GF" = "Mixed", 
+    "H" = "Asian", 
+    "J" = "Asian",
+    "K" = "Asian", 
+    "L" = "Asian", 
+    "LA" = "Asian", 
+    "LB" = "Asian", 
+    "LC" = "Asian",
+    "LD" = "Asian", 
+    "LE" = "Asian", 
+    "LF" = "Asian", 
+    "LG" = "Asian", 
+    "LH" = "Asian",
+    "LJ" = "Asian", 
+    "LK" = "Asian", 
+    "R" = "Asian", 
+    "M" = "Black", 
+    "N" = "Black", 
+    "P" = "Black",
+    "PA" = "Black", 
+    "PB" = "Black", 
+    "PC" = "Black", 
+    "PD" = "Black", 
+    "PE" = "Black",
+    "S" = "Other", 
+    "SA" = "Other", 
+    "SB" = "Other", 
+    "SC" = "Other",
+    "SD" = "Other", 
+    "SE" = "Other"
+  )
+  
+  df$ETHNICITY <- as.character(df$ETHNICITY)
+  
+  df$Grouped_Ethinicity <- ethnicity_mapping[df$ETHNICITY]
+  
+  return(df)
+}
 
+
+
+
+extended_summary <- function(df) {
+  df_sum <- data.frame(
+    Columns = colnames(df),
+    Missings_val = sapply(df, function(x) sum(is.na(x))),
+    Unique_val = sapply(df, function(x) length(unique(x))),
+    Class_val = sapply(df, class)
+  )
+  print(df_sum)
+  return(df_sum)
+}
+
+#' Calculate Survival Days
+#'
+#' This function calculates the number of days between the diagnosis date 
+#' and the vital status date for each row in the data frame. 
+#'
+#' @param df A data frame containing the necessary columns: 'DIAGNOSISDATEBEST', 
+#' 'VITALSTATUSDATE', and 'VITALSTATUS'.
+#'
+#' @return The input data frame with an additional column 'date_diff', 
+#' representing the number of days between 'DIAGNOSISDATEBEST' and 'VITALSTATUSDATE'.
+#' @export
+#'
+#' @examples
+#' # Assuming you have a merged dataset `merged_data`
+#' survival_days(merged_data)
+survival_days <- function(df) {
+  message("Please make sure to merge 'sim_av_patient' and 'sim_av_tumour'")
+  
+  required_columns <- c("DIAGNOSISDATEBEST", "VITALSTATUSDATE", "VITALSTATUS")
+  if (!all(required_columns %in% colnames(df))) {
+    stop(paste(
+      "The input data frame must contain the following columns:",
+      paste(required_columns, collapse = ", ")
+    ))
+  }
+  
+  df$DIAGNOSISDATEBEST <- as.Date(df$DIAGNOSISDATEBEST)
+  df$VITALSTATUSDATE <- as.Date(df$VITALSTATUSDATE)
+  
+  df$diff_date <- difftime(df$VITALSTATUSDATE, df$DIAGNOSISDATEBEST, units = "days")
+  df$date_to_death <- ifelse(df$VITALSTATUS == "D", df$diff_date, NA)
+  
+  return(df)
+}
