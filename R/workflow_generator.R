7f372c08393369ab200498a4a95b2cc473456941
p90j <jakob1741@hotmail.com>
2025-02-04 03:59:11 +0100

    added new package built

--- R/workflow_generator.R
+++ R/workflow_generator.R
@@ -18,7 +18,7 @@
 #'
 #' @return None. The function writes the generated workflow script to the specified file path and outputs a message with the location of the created script.
 #'
-#' @importFrom simulacrumR sqlite2oracle 
+#' @importFrom simulacrumWorkflowR sqlite2oracle 
 #'
 #' @export
 #'
