0dbce67eb2ef231200fb5d84efbf31a8116ab5b4
p90j <jakob1741@hotmail.com>
2025-02-25 13:28:45 +0100

    bug fix in sqlite2oracle()

--- R/sqlite2oracle.R
+++ R/sqlite2oracle.R
@@ -36,7 +36,12 @@ sqlite2oracle <- function(query) {
     if (!is.character(query)) stop("Please make sure input query is a string")
     
     # Convert main SQL keywords to uppercase
-    keywords <- c("SELECT", "FROM", "WHERE", "INNER", "OUTER", "JOIN", "RIGHT", "LEFT", "ORDER BY", "GROUP BY", "HAVING")
+    keywords <- c("ADD", "ADD CONSTRAINT", "ALL", "ALTER", "ALTER", "ALTER TABLE", "AND", "ANY", "AS", "ASC", "BACKUP DATABASE", "BETWEEN CASE", "CHECK", "COLUMN", "CONSTRAINT",
+                  "CREATE", "CREATE DATABASE", "CREATE INDEX", "CREATE OR REPLACE VIEW", "CREATE TABLE", "CREATE PROCEDURE", "CREATE UNIQUE INDEX", "CREATE VIEW", "DATABASE",
+                  "DEFAULT", "DELETE", "DESC", "DISTINCT", "DROP", "DROP COLUMN", "DROP CONSTRAINT", "DROP DATABASE", "DROP DEFAULT", "DROP INDEX", "DROP TABLE", "DROP VIEW",
+                  "EXEC", "EXISTS", "FOREIGN KEY", "FROM", "FULL OUTER JOIN", "GROUP BY", "HAVING", "IN", "INDEX", "INNER JOIN", "INSERT INTO", "INSERT INTO SELECT", "IS NULL",
+                  "IS NOT NULL", "JOIN", "LEFT JOIN", "LIKE", "LIMIT", "NOT", "NOT NULL", "OR", "ORDER BY", "OUTER JOIN", "PRIMARY KEY", "PROCEDURE", "RIGHT JOIN", "ROWNUM",
+                  "SELECT", "SELECT DISTINCT", "SELECT INTO", "SELECT TOP", "SET", "TABLE", "TOP", "TRUNCATE TABLE", "UNION", "UNIQUE", "UPDATE", "VALUES", "VIEW", "WHERE")
     for (keyword in keywords) {
       query <- gsub(keyword, toupper(keyword), query, ignore.case = TRUE)
     }
@@ -56,3 +61,4 @@ sqlite2oracle <- function(query) {
     
     return(query)
 }
+
